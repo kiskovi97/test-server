ab91f83e909342a883e2b663b94d36f5721410a1
GergoKovacsBookr <kiskovi97@gmail.com>
2021-10-27 11:23:20 +0200

    30 second wait

--- routes/callback.js
+++ routes/callback.js
@@ -42,7 +42,7 @@ router.get('/', async function(req, res, next) {
         const timeout = setTimeout(() => {
           source.cancel();
           // Timeout Logic
-        }, 5000);
+        },  30*1000);
         
 
 
