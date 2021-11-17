ccdad355d61c65ab47e3556b00397a68442fb983
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-11-17 10:17:22 +0100

    HashFile

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test Server' });
+  res.Send('af35d1e9-135e-4816-a9f0-e9e366158454');
 });
 
 module.exports = router;
