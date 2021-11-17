f342c85588398e9237762ecb44578b72f242e9a0
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-11-17 10:21:03 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.Send('af35d1e9-135e-4816-a9f0-e9e366158454');
+  res.send('af35d1e9-135e-4816-a9f0-e9e366158454');
 });
 
 module.exports = router;
